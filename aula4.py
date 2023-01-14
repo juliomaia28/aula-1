165e84b5a4c8283a9fb6ad925cc9214ee185bf77
JulioMaia17 <juliomaia2004@gmail.com>
2023-01-14 16:25:07 -0300

    subindo aula4

---
+++ aula4.py
@@ -0,0 +1,4 @@
+time_que_torce =input("qual o time que voce torce?")
+print(f'time que eu torco e:{time_que_torce}')
+cor_da_camisa =input("qual a cor da sua camisa?")
+print(f'a cor da minha camisa e:{cor_da_camisa}')
