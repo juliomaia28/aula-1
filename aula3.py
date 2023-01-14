a899a0ec6281decbce18c6db8c1a5fa7a43e532c
JulioMaia17 <juliomaia2004@gmail.com>
2023-01-14 16:19:42 -0300

    subindo aula3

---
+++ aula3.py
@@ -0,0 +1,4 @@
+cor_dos_olhos =input('qual a cor dos seus olhos?')
+print(f"cor dos seus olhos sao:{cor_dos_olhos}")
+cor_da_pele =input('qual a cor da sua pele?')
+print(f'cor da sua pele e:{cor_da_pele}')
